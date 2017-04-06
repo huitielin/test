5de0ec65b50971bd823d3bb3d3093da435dca771
huitielin <itjy066@126.com>
2017-04-06 10:25:38 +0800

    a

---
+++ js/index.js
@@ -0,0 +1,75 @@
+/**
+ * Created by 87644 on 2017/4/1.
+ */
+$(function(){
+    $("#box .item .the1").hover(
+        function(){
+            $(this).children(".the1_a").css("display","block")
+        },
+        function(){
+            $(this) .children(".the1_a").css("display","none")
+
+
+        }
+    )
+})
+$(function(){
+    $("#box .item .the2").hover(
+        function(){
+            $(this).children(".the2_a").css("display","block")
+        },
+        function(){
+            $(this) .children(".the2_a").css("display","none")
+
+
+        }
+    )
+})
+$(function(){
+    $("#box .item .the3").hover(
+        function(){
+            $(this).children(".the3_a").css("display","block")
+        },
+        function(){
+            $(this) .children(".the3_a").css("display","none")
+
+
+        }
+    )
+})
+$(function(){
+    $("#box .item .the4").hover(
+        function(){
+            $(this).children(".the4_a").css("display","block")
+        },
+        function(){
+            $(this) .children(".the4_a").css("display","none")
+
+
+        }
+    )
+})
+$(function(){
+    $("#box .item .the5").hover(
+        function(){
+            $(this).children(".the5_a").css("display","block")
+        },
+        function(){
+            $(this) .children(".the5_a").css("display","none")
+
+
+        }
+    )
+})
+$(function(){
+    $("#box .item .the6").hover(
+        function(){
+            $(this).children(".the6_a").css("display","block")
+        },
+        function(){
+            $(this) .children(".the6_a").css("display","none")
+
+
+        }
+    )
+})
